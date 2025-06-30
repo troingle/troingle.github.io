b0945c8c5e3fa312df4e9ad554aa8c61fa01d4b9
troingle <109359272+troingle@users.noreply.github.com>
2025-06-30 19:55:04 +0300

    Added existing files

---
+++ script.js
@@ -0,0 +1,25 @@
+var dark = false;
+
+document.addEventListener("DOMContentLoaded", function() {
+  const button = document.getElementById("theme");
+
+  button.addEventListener("click", function() {
+    dark = !dark;
+    if (dark){ // change to dark
+        document.body.style.backgroundColor = "#2c2c2a";
+        var links = document.querySelectorAll('a');
+        links.forEach(link => {
+            link.style.color = "#fc8181";
+        });
+        document.getElementById("theme").style.color = "#fc8181";
+    }
+    else { // change to light
+        document.body.style.backgroundColor = "#979bc7";
+        var links = document.querySelectorAll('a');
+        links.forEach(link => {
+            link.style.color = "#f3c0ce";
+        });
+        document.getElementById("theme").style.color = "#f3c0ce";
+    }
+  });
+});
